3a59d5205491965b029b236603314f493108bb6c
nicolaDeCristofaro <nico.de.cristofaro97@gmail.com>
2021-02-01 12:46:53 +0100

    adding DB tables

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ var app = require('express')(); // Create an instance of an Express app
 
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
-mobileApp.tables.add('Book'); // Create a table for 'Book' with default settings
-mobileApp.tables.add('Users'); // Create a table for 'Book' with default settings
+mobileApp.tables.add('Users');
+mobileApp.tables.add('Reviews');
+mobileApp.tables.add('Keywords');
+mobileApp.tables.add('Favors');
+mobileApp.tables.add('Favors_Keywords');
+mobileApp.tables.add('Favors_Candidates');
+
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
